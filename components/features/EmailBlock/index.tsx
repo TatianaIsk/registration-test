1312e2329007f61fab49e01190f35ebd1f5fdceb
Татьяна <isskova.tatyana@yandex.ru>
2023-12-01 18:48:04 +0300

    feat: add mask for phone number

--- components/features/EmailBlock/index.tsx
+++ components/features/EmailBlock/index.tsx
@@ -1,23 +1,29 @@
+import MaskedInput from 'react-input-mask';
+
 import Label from '@/components/ui/Label';
+import Input from '@/components/ui/Input';
 
 import s from './EmailBlock.module.scss';
-import Input from '@/components/ui/Input';
 
 const EmailBlock = () => {
   return (
     <div className={s.wrapper}>
       <div className={s.input}>
         <Label>Номер телефона</Label>
-        <Input />
+        <MaskedInput
+          mask="+7 (999) 999-99-99"
+          alwaysShowMask
+          className={s.inputPhone}
+        />
       </div>
       <div className={s.input}>
         <Label>Электронная почта</Label>
-        <Input />
+        <Input placeholder='Введите email'/>
       </div>
       <div className={s.inputCheckboxForm}>
         <Label>Я согласен</Label>
-        <Input type='checkbox' className={s.inputCheckbox}/>
-				<p className={s.text}>принимать актуальную информацию на емейл</p>
+        <Input type='checkbox' className={s.inputCheckbox} />
+        <p className={s.text}>принимать актуальную информацию на емейл</p>
       </div>
     </div>
   );
